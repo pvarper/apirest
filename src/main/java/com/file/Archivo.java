361dd41c348e3fd3a49889128528e00c07ab7dd8
pvarper <p.varper@gmail.com>
2019-09-04 18:05:40 -0400

    Creacion de la clase Cliente y la clase Archivo

---
+++ src/main/java/com/file/Archivo.java
@@ -0,0 +1,24 @@
+package com.file;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.ws.rs.core.Response;
+
+import com.entity.Cliente;
+
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
+public class Archivo {
+
+	
+
+}
