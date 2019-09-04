e232cd3f77f8021f6eed1e2f37c4a6f76dc8bc80
pvarper <p.varper@gmail.com>
2019-09-04 18:27:02 -0400

    Se refactorizo el constructor de Archivo para la creacion del Direcotrio ficheros y el archivo usuarios, test:creacionDirectorioArchivoTest, ok

--- src/main/java/com/file/Archivo.java
+++ src/main/java/com/file/Archivo.java
@@ -1,6 +1,7 @@
 package com.file;
 
 import java.io.File;
+import java.io.FileWriter;
 
 import lombok.extern.jbosslog.JBossLog;
 
@@ -12,7 +13,22 @@ public class Archivo {
 	File file;
 	
 	public Archivo() {
-		
+		try {
+			log.info("si el directorio "+nombreDirectorio+" no existe, se procede a crearlo");
+			this.file = new File(nombreDirectorio);
+			if (!this.file.exists()) {
+				this.file.mkdir();
+				log.info("se creo el directorio "+nombreDirectorio);
+				log.info("se va crear el archivo "+nombreArchivoUsuarios);
+				FileWriter flwriter = new FileWriter(this.file.getAbsolutePath() + "\\" + nombreArchivoUsuarios);
+				log.info("se creo el archivo "+nombreArchivoUsuarios);
+				flwriter.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("[Archivo] error al crear el directorio y el archivo usuarios",e);
+		}
 		
 	}
 
